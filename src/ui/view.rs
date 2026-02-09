08920b4e4dd0265e458ab8870f8d25b5e957d4b5
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2026-02-09 07:24:21 +0100

    chore: updated menu messages

--- src/ui/view.rs
+++ src/ui/view.rs
@@ -1,5 +1,12 @@
 pub fn display_actions() {
-    println!(
-        "1 -> View all expenses\n2 -> Add new expense\n3 -> Edit expense\n4 -> View single expense\n5 -> Delete expense\nq -> Quit"
-    );
+    println!("\n====================================");
+    println!("        EXPENSE TRACKER MENU        ");
+    println!("====================================");
+    println!(" [1] View all expenses");
+    println!(" [2] Add new expense");
+    println!(" [3] Edit expense");
+    println!(" [4] View single expense");
+    println!(" [5] Delete expense");
+    println!(" [q] Quit");
+    println!("====================================");
 }
